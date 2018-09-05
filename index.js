08545a98f2da0464bc76339f31047237360ef15c
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-05 15:28:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ class Customer {
   meals() {
     const a = []
   	for (const meal of store.meals) {
-    	if (meal.deliveryId === this.deliveryId) {
+    	while (meal.deliveryId === this.deliveryId) {
     	   a.push(meal.title)
 	    }
 	    return a
