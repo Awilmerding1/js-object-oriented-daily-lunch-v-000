5becc9d109bed5861c957cd99b532b1faa1988fe
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-05 16:02:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ class Meal {
   }
   
   static byPrice() {
-    let prices = this.map(function(meal) {
+    let prices = Meal.map(function(meal) {
       return meal.price
     })
     return prices.sort(function(price1, price2){
